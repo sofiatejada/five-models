6262d393a4fda30ddd595b296c7823f1c51a3496
Sofia Tejada <sofianais@icloud.com>
2021-07-26 10:08:50 -0700

    write all test routes for holidays

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Pokemon from '../lib/models/HiddenAbility.js';
+import Holiday from '../lib/models/Holiday.js';
 
 
 describe('pokemon routes', () => {
@@ -118,6 +119,73 @@ describe('holiday routes', () => {
     expect(res.body).toEqual({
       id: '1',
       ...korea
+    });
+  });
+
+  it('gets a holiday by id with GET', async () => {
+    const spain = await Holiday.insert({
+      country: 'Spain',
+      lang: 'Spanish',
+      capital: 'Madrid',
+    });
+
+    const res = await request(app).get(`/api/v1/holidays/${spain.id}`);
+
+    expect(res.body).toEqual(spain);
+  });
+
+  it('gets all holidays with GET', async () => {
+    const spain = await Holiday.insert({
+      country: 'Spain',
+      lang: 'Spanish',
+      capital: 'Madrid',
+    });
+
+    const korea = {
+      country: 'South Korea',
+      lang: 'Korean',
+      capital: 'Seoul',
+    };
+
+    const australia = {
+      country: 'Australia',
+      lang: 'English',
+      capital: 'Canberra',
+    };
+
+    return request(app)
+      .get('/api/v1/holidays')
+      .then((res) => {
+        expect(res.body).toEqual([spain, korea, australia]);
+      });
+
+  });
+
+  it('updates a holiday by id with PUT', async () => {
+    const australia = await Holiday.insert({
+      country: 'Australia',
+      lang: 'English',
+      capital: 'N/A',
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/holidays/${australia.id}`)
+      .send({ capital: 'Canberra' });
+
+    expect(res.body).toEqual({ ...australia, capital: 'Canberra' });
+  });
+
+  it('deletes a holiday by id with DELETE', async () => {
+    const australia = await Holiday.insert({
+      country: 'Australia',
+      lang: 'English',
+      capital: 'Canberra',
+    });
+
+    const res = await request(app).delete(`/api/v1/holidays/${australia.id}`);
+
+    expect(res.body).toEqual({
+      message: `Holiday ${australia.country} has been deleted.`
     });
   });
   
