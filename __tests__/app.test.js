6cdf5412001f268c8e093d5ce0e81bd741fc5837
Sofia Tejada <sofianais@icloud.com>
2021-07-26 10:37:10 -0700

    all test routes written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,9 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Pokemon from '../lib/models/HiddenAbility.js';
 import Holiday from '../lib/models/Holiday.js';
+import Book from '../lib/models/Book';
+import Cat from '../lib/models/Cat';
+import VideoGame from '../lib/models/VideoGame';
 
 
 describe('pokemon routes', () => {
@@ -191,26 +194,269 @@ describe('holiday routes', () => {
   
 });
 
-// describe('book routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
+  it('creates a book via POST', async () => {
+    const frankenstein = { 
+      name: 'frankenstein',
+      author: 'mary shelley',
+      genre: 'science fiction',
+    };
+    const res = await request(app)
+      .post('/api/v1/book')
+      .send(frankenstein);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...frankenstein
+    });
+  });
+
+  it('gets a book by id with GET', async () => {
+    const frankenstein = await Book.insert({ 
+      name: 'frankenstein',
+      author: 'mary shelley',
+      genre: 'science fiction',
+    });
+
+    const res = await request(app).get(`/api/v1/pokemon/${frankenstein.id}`);
+
+    expect(res.body).toEqual(frankenstein);
+  });
+
+  it('gets all book with GET', async () => {
+    const frankenstein = await Book.insert({ 
+      name: 'frankenstein',
+      author: 'mary shelley',
+      genre: 'science fiction',
+    });
+
+    const wotw = await Book.insert({ 
+      name: 'war of the worlds', 
+      author: 'h.g. wells',
+      genre: 'science fiction',
+    });
+
+    const annihilation = await Book.insert({
+      name: 'annihilation',
+      author: 'jeff vandermeer',
+      genre: 'science fiction',
+    });
+
+    return request(app)
+      .get('/api/v1/books')
+      .then((res) => {
+        expect(res.body).toEqual([frankenstein, wotw, annihilation]);
+      });
+  });
+
+  it('updates a book by id with PUT', async () => {
+    const frankenstein = await Book.insert({ 
+      name: 'frankenstein',
+      author: 'mary shelley',
+      genre: 'science fiction',
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/books/${frankenstein.id}`)
+      .send({ type2: 'grass' });
+    
+    expect(res.body).toEqual({ ...frankenstein, genre: 'horror' });
+  });
+
+  it('deletes a book by id with DELETE', async () => {
+    const wotw = await Book.insert({ 
+      name: 'war of the worlds', 
+      author: 'h.g. wells',
+      genre: 'science fiction',
+    });
+
+    const res = await request(app).delete(`/api/v1/books/${wotw.id}`);
+
+    expect(res.body).toEqual({
+      message: `Book ${wotw.name} has been deleted.`
+    });
+  });
+});
+
+describe('cat routes routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a cat via POST', async () => {
+    const luna = { 
+      name: 'luna',
+      age: 3,
+      breed: 'ragdoll',
+    };
+
+    const res = await request(app)
+      .post('/api/v1/cats')
+      .send(luna);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...luna
+    });
+  });
+
+  it('gets a cat by id with GET', async () => {
+    const luna = await Cat.insert({ 
+      name: 'luna',
+      age: 3,
+      breed: 'ragdoll',
+    });
+
+    const res = await request(app).get(`/api/v1/cats/${luna.id}`);
+
+    expect(res.body).toEqual(luna);
+  });
+
+  it('gets all cats with GET', async () => {
+    const luna = await Cat.insert({ 
+      name: 'luna',
+      age: 3,
+      breed: 'ragdoll',
+    });
+
+    const mila = await Cat.insert({ 
+      name: 'mila',
+      age: 8,
+      breed: 'tabby',
+    });
+
+    const shadow = await Cat.insert({ 
+      name: 'shadow',
+      age: 1,
+      breed: 'black tabby',
+    });
+
+    return request(app)
+      .get('/api/v1/cats')
+      .then((res) => {
+        expect(res.body).toEqual([luna, mila, shadow]);
+      });
+  });
+
+  it('updates a cat by id with PUT', async () => {
+    const luna = await Cat.insert({ 
+      name: 'luna',
+      age: 3,
+      breed: 'ragdoll',
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/cats/${luna.id}`)
+      .send({ age: 4 });
+    
+    expect(res.body).toEqual({ ...luna, age: 4 });
+  });
+
+  it('deletes a cat by id with DELETE', async () => {
+    const shadow = await Cat.insert({ 
+      name: 'shadow',
+      age: 1,
+      breed: 'black tabby',
+    });
+
+    const res = await request(app).delete(`/api/v1/cats/${shadow.id}`);
+
+    expect(res.body).toEqual({
+      message: `Cat ${shadow.name} has been deleted.`
+    });
+  });
+});
+
+describe('video game routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a videogame via POST', async () => {
+    const subnautica = { 
+      name: 'subnautica',
+      genre: 'survival',
+      rating: 5,
+    };
+
+    const res = await request(app)
+      .post('/api/v1/videogames')
+      .send(subnautica);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...subnautica
+    });
+  });
+
+  it('gets a videogame by id with GET', async () => {
+    const subnautica = await VideoGame.insert({ 
+      name: 'subnautica',
+      genre: 'survival',
+      rating: 5,
+    });
+
+    const res = await request(app).get(`/api/v1/videogames/${subnautica.id}`);
+
+    expect(res.body).toEqual(subnautica);
+  });
+
+  it('gets all videogames with GET', async () => {
+    const subnautica = await VideoGame.insert({ 
+      name: 'subnautica',
+      genre: 'survival',
+      rating: 5,
+    });
+
+    const sims4 = await VideoGame.insert({ 
+      name: 'sims 4',
+      genre: 'simulation',
+      rating: 3,
+    });
+
+    const pokemon = await VideoGame.insert({ 
+      name: 'pokemon sword',
+      genre: 'rpg',
+      rating: 4,
+    });
+
+    return request(app)
+      .get('/api/v1/videogames')
+      .then((res) => {
+        expect(res.body).toEqual([subnautica, sims4, pokemon]);
+      });
+  });
+
+  it('updates a videogame by id with PUT', async () => {
+    const subnautica = await VideoGame.insert({ 
+      name: 'subnautica',
+      genre: 'survival',
+      rating: 5,
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/videogames/${subnautica.id}`)
+      .send({ genre: 'action adventure' });
+    
+    expect(res.body).toEqual({ ...subnautica, genre: 'action adventure' });
+  });
+
+  it('deletes a videogame by id with DELETE', async () => {
+    const sims4 = await VideoGame.insert({ 
+      name: 'sims 4',
+      genre: 'simulation',
+      rating: 3,
+    });
+
+    const res = await request(app).delete(`/api/v1/videogames/${sims4.id}`);
+
+    expect(res.body).toEqual({
+      message: `Video Game ${sims4.name} has been deleted.`
+    });
+  });
   
-// });
-
-// describe('cat routes routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
-
-  
-// });
-
-// describe('video game routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
-
-  
-// });
+});
